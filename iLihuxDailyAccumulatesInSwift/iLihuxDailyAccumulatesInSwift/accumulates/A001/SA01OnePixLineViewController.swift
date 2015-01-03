615f40c01588c80972a6344c5e0384de56b7b4f8
李辉 <hui.li@renren-inc.com>
2015-01-03 08:51:41 +0800

    清理swift工程中的代码：make it simple and stupid !

--- iLihuxDailyAccumulatesInSwift/iLihuxDailyAccumulatesInSwift/accumulates/A001/SA01OnePixLineViewController.swift
+++ iLihuxDailyAccumulatesInSwift/iLihuxDailyAccumulatesInSwift/accumulates/A001/SA01OnePixLineViewController.swift
@@ -8,62 +8,10 @@
 
 import UIKit
 
-class SA01OnePixLineViewController: UIViewController {
+class SA01OnePixLineViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var resultLabel: UILabel!
-
-    let kMinimumValideCount = 8
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    @IBAction func textFieldDidChanged(sender: AnyObject)
-    {
-        self.resultLabel.text = "解析结果是：\(self.textField.text)"
-    }
-
-    func cookString(original: String) -> String
-    {
-        if original.utf16Count < self.kMinimumValideCount {
-            return original
-        }
-
-        var i = 0, atIndex = -1, leftParenthesis = -1, rightParenthesisIndex = -1
-        for character in original {
-            if atIndex == -1 {
-                atIndex = character == "@" ? i : -1
-                continue
-            }
-            if leftParenthesis == -1 {
-                leftParenthesis = character == "(" ? i : -1
-                continue
-            }
-            if rightParenthesisIndex == -1 {
-                if character == ")" {
-                    println("找到一个：--\(atIndex)--\(leftParenthesis)--\(rightParenthesisIndex)")
-                }
-            }
-        }
-//        var i = 0, startName = -1, endName = -1
-//        var getName = false
-//        var getUserID = false
-//        for character in original {
-//            if startName == -1 {
-//                startName = character == "@" ? i : -1
-//                endName = startName
-//            } else {
-//                if !getName {
-//                    if character != "(" {
-//                        endName = endName + 1
-//                    } else {
-//                        getName = true
-//                    }
-//                } else {
-//                }
-//            }
-//        }
-        return original
     }
 
     override func didReceiveMemoryWarning() {
