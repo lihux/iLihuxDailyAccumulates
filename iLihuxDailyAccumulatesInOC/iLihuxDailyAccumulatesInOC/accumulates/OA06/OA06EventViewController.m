8efb0833a8436739585f59ed48de7c16175797fe
李辉 <hui.li@renren-inc.com>
2015-01-12 20:12:49 +0800

    添加06 uiview 事件响应链

--- iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA06/OA06EventViewController.m
+++ iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA06/OA06EventViewController.m
@@ -13,6 +13,8 @@
 @property (weak, nonatomic) IBOutlet UIView *orangeView;
 @property (weak, nonatomic) IBOutlet UIButton *button;
 @property (weak, nonatomic) IBOutlet UILabel *label;
+@property (nonatomic, strong) UIView *greenView;
+@property (nonatomic, strong) UIView *yellowView;
 
 @end
 
@@ -34,6 +36,37 @@
     [self printResponder:@"self.view" responder:self.view];
     [self printResponder:@"self.button" responder:self.button];
     [self printResponder:@"self.label" responder:self.label];
+}
+
+- (IBAction)didTapOnButton:(id)sender
+{
+    if (self.button.tag == 0) {
+        self.greenView = [[UIView alloc] initWithFrame:CGRectMake(30, 90, 200, 200)];
+        self.yellowView = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 40, 40)];
+        self.greenView.backgroundColor = [UIColor greenColor];
+        self.yellowView.backgroundColor = [UIColor yellowColor];
+        [self.greenView addSubview:self.yellowView];
+        [self.view addSubview:self.greenView];
+        self.button.tag = 1;
+    } else if (self.button.tag == 1) {
+        [UIView animateWithDuration:0.8 animations:^{
+            CGRect bounds = self.yellowView.bounds;
+            bounds.size.width = bounds.size.width / 2;
+            self.yellowView.bounds = bounds;
+        } completion:^(BOOL finished) {
+            NSLog(@"看看吧：%@", self.yellowView);
+            self.button.tag = 2;
+        }];
+    } else if (self.button.tag == 2) {
+        [UIView animateWithDuration:0.8 animations:^{
+            CGRect bounds = self.greenView.bounds;
+            bounds.origin.x = 20;
+            bounds.origin.y = -20;
+            self.greenView.bounds = bounds;
+        } completion:^(BOOL finished) {
+            NSLog(@"看看吧：%@,dlkdkldssd %@", self.yellowView, self.greenView);
+        }];
+    }
 }
 
 - (void)printResponder:(NSString *)info responder: (UIResponder *)responder
