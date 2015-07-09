54cea85795a0203b9976126439a65c76aca8542a
李辉 <lihui.zl@163.com>
2015-07-09 16:56:26 +0800

    OA10-SDWebImage的使用

---
+++ iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA10/OA10SDWebImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  OA10SDWebImageViewController.h
+//  iLihuxDailyAccumulatesInOC
+//
+//  Created by lihui on 15/7/2.
+//  Copyright © 2015年 lihux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OA10SDWebImageViewController : UIViewController
+
+@end
---
+++ iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA10/OA10SDWebImageViewController.m
@@ -0,0 +1,25 @@
+//
+//  OA10SDWebImageViewController.m
+//  iLihuxDailyAccumulatesInOC
+//
+//  Created by lihui on 15/7/2.
+//  Copyright © 2015年 lihux. All rights reserved.
+//
+
+#import "OA10SDWebImageViewController.h"
+
+@interface OA10SDWebImageViewController ()
+
+@end
+
+@implementation OA10SDWebImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
