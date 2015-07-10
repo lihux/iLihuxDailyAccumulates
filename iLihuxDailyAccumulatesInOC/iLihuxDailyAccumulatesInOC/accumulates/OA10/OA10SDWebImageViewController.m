ea98d6d09a420daf5a7757c0453ddb0d34f5e903
李辉 <lihui.zl@163.com>
2015-07-10 09:28:59 +0800

    添加OA10网络请求方法

--- iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA10/OA10SDWebImageViewController.m
+++ iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA10/OA10SDWebImageViewController.m
@@ -10,12 +10,28 @@
 
 @interface OA10SDWebImageViewController ()
 
+@property (nonatomic, strong) NSURLSession *session;
+
 @end
 
 @implementation OA10SDWebImageViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self loadImagesFromAliYunServer];
+}
+
+- (void)loadImagesFromAliYunServer
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://120.24.70.94:4000/superCarPhotos/superCar1.jpg"]];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    NSURLSessionTask *task = [session dataTaskWithURL:imageURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSLog(@"%@", data);
+    }];
+    [task resume];
+//    [self.session dataTaskWithURL:imageURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//        NSLog(@"收到数据：%@, \n********************\n response = %@ \n\n\n error = %@", data, response, error);
+//    }];
 }
 
 - (void)didReceiveMemoryWarning {
