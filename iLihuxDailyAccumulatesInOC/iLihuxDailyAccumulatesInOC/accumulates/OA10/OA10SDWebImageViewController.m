79149fe6e03883e728b0255c9857c4ed593a39e9
李辉 <lihui.zl@163.com>
2015-07-13 11:22:43 +0800

    整理OA10-网络请求使用的代码，去掉注释

--- iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA10/OA10SDWebImageViewController.m
+++ iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA10/OA10SDWebImageViewController.m
@@ -10,7 +10,6 @@
 
 @interface OA10SDWebImageViewController ()
 
-@property (nonatomic, strong) NSURLSession *session;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
@@ -24,28 +23,17 @@
 
 - (void)loadImagesFromAliYunServer
 {
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://120.24.70.94:4000/superCarPhotos/superCar1.jpg"]];
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-        self.imageView.image = [UIImage imageWithData:data];
-        NSLog(@"%@", data);
+    NSURLSessionTask *task = [[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:@"http://120.24.70.94:4000/superCarPhotos/superCar1.jpg"] completionHandler:^(NSData * __nullable data, NSURLResponse * __nullable response, NSError * __nullable error) {
+        if (error) {
+            NSLog(@"%@", error);
+        } else {
+            NSLog(@"%@", data);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.imageView.image = [UIImage imageWithData:data];
+            });
+        }
     }];
-    
-//    request.HTTPMethod = @"get";
-//    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-//    NSURLSessionTask *sessionTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-//        NSLog(@"%@", data);
-//    }];
-//    [sessionTask resume];
-//    NSMutableURLRequest *request = [nsmuta requestWithURL:[NSURL URLWithString:@"http://120.24.70.94:4000/superCarPhotos/superCar1.jpg"]];
-//    request.HTTPMethod = @"get";
-//    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-//    NSURLSessionTask *task = [session dataTaskWithURL:imageURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-//        NSLog(@"%@", data);
-//    }];
-//    [task resume];
-//    [self.session dataTaskWithURL:imageURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-//        NSLog(@"收到数据：%@, \n********************\n response = %@ \n\n\n error = %@", data, response, error);
-//    }];
+    [task resume];
 }
 
 - (void)didReceiveMemoryWarning {
