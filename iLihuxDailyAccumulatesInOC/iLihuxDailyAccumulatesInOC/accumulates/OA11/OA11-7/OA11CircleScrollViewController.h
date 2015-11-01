3bb8b4452027b0d99dd7ff2919a59ffcff19b51c
李辉 <lihui.zl@163.com>
2015-11-01 21:59:18 +0800

    定制滚筒化的下拉刷新

---
+++ iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA11/OA11-7/OA11CircleScrollViewController.h
@@ -0,0 +1,13 @@
+//
+//  OA11CircleScrollViewController.h
+//  iLihuxDailyAccumulatesInOC
+//
+//  Created by lihui on 15/9/15.
+//  Copyright (c) 2015年 lihux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OA11CircleScrollViewController : UIViewController
+
+@end
---
+++ iLihuxDailyAccumulatesInOC/iLihuxDailyAccumulatesInOC/accumulates/OA11/OA11-7/OA11CircleScrollViewController.m
@@ -0,0 +1,37 @@
+//
+//  OA11CircleScrollViewController.m
+//  iLihuxDailyAccumulatesInOC
+//
+//  Created by lihui on 15/9/15.
+//  Copyright (c) 2015年 lihux. All rights reserved.
+//
+
+#import "OA11CircleScrollViewController.h"
+
+@interface OA11CircleScrollViewController ()
+
+@end
+
+@implementation OA11CircleScrollViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
